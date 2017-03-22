aaae33f7886ce90ed45877700760134ce54ee107
sdp <809263087@qq.com>
2017-03-23 00:06:47 +0800

    上传第一次

--- app/src/main/java/com/sdp/panda/gaodemaptestdemo/MainActivity.java
+++ app/src/main/java/com/sdp/panda/gaodemaptestdemo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.sdp.panda.gaodemaptestdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+
+    @InjectView(R.id.btn_last)
+    Button btnLast;
+    @InjectView(R.id.btn_wakeUp)
+    Button btnWakeUp;
+    @InjectView(R.id.btn_weilan)
+    Button btnWeilan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
+
+    @OnClick({R.id.btn_last, R.id.btn_wakeUp,R.id.btn_weilan})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_last:
+                startActivity(new Intent(this, LasLocationActivity.class));
+                break;
+            case R.id.btn_wakeUp:
+                startActivity(new Intent(this, UpCpuLocationActivity.class));
+                break;
+            case R.id.btn_weilan:
+                startActivity(new Intent(this, WeiLanLocationActivity.class));
+                break;
+        }
+    }
+
 }
